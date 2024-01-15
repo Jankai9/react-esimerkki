469b567f9a5ac386fee3194667bcc66d1ddeff7c
Janne Kaipala <janne.kaipala@hiq.fi>
2024-01-15 16:05:02 +0200

    poistettu x

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Hederi } from './Hederi';
 import { Tuote } from './Tuote';
 import { TuoteLista } from './Tuotelista';
 import logo from './logo.svg';
-// ximport './App.css';
+// import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
